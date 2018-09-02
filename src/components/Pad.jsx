31ef990cb8f3030b18701364602949b9c8a0d05a
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-02 16:36:07 +0300

    Change Pad component to stateful component and add handleClick method

--- src/components/Pad.jsx
+++ src/components/Pad.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
+import "./Pad.css"
 
-export default (props) => {
-    return (
-        <div className="drum-pad">
-            <p>{props.button}</p>
-            <audio src="" className="clip"></audio>
-        </div>
-    )
+export default class Pad extends Component {
+    handleClick = (event) => {
+        console.log(event.target);
+    }
+    render() {
+        return (
+            <div className="drum-pad" onClick={this.handleClick}>
+                <p>{this.props.button}</p>
+                <audio src="" className="clip"></audio>
+            </div>
+        )
+    }
 }
