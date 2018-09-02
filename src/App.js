b4f548fc1a8b62a0d0fd65ff553459b2f2087300
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-02 15:48:19 +0300

    Add the main component called DrumMachine and add it to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import DrumMachine from './components/DrumMachine'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <DrumMachine>
+        </DrumMachine>
       </div>
     );
   }
