10c2ce3b68ec544da966fbc4313c837e53286f87
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-03 09:07:09 +0300

    Add fre code camp test script and modify code to pass all tests

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 import DrumMachine from './components/DrumMachine'
-import Pad from './components/Pad'
+import DrumPad from './components/Pad'
 
 class App extends Component {
 
   handleKeyDown = (e) => {
-    let key = document.getElementsByName(e.key.toLowerCase())
+    let keysArr = ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c']
+    let key = null
+    if (keysArr.includes(e.key)) {
+      key = document.getElementsByName(e.key)
+    } else {
+      return
+    }
     if (key[0]) {
+      let display = document.getElementById("display")
+      display.innerText = e.key
       key[0].play()
     }
   }
@@ -22,16 +30,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div id="display"></div>
         <DrumMachine>
-          <Pad button="q" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" clicked={this.clickHandler} />
-          <Pad button="w" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" clicked={this.clickHandler} />
-          <Pad button="e" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" clicked={this.clickHandler} />
-          <Pad button="a" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" clicked={this.clickHandler} />
-          <Pad button="s" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" clicked={this.clickHandler} />
-          <Pad button="d" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" clicked={this.clickHandler} />
-          <Pad button="z" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" clicked={this.clickHandler} />
-          <Pad button="x" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" clicked={this.clickHandler} />
-          <Pad button="c" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" clicked={this.clickHandler} />
+          <DrumPad button="q" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" clicked={this.clickHandler} desc="Heater-1" />
+          <DrumPad button="w" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" clicked={this.clickHandler} desc="Heater-2" />
+          <DrumPad button="e" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" clicked={this.clickHandler} desc="Heater-3" />
+          <DrumPad button="a" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" clicked={this.clickHandler} desc="Heater-4" />
+          <DrumPad button="s" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" clicked={this.clickHandler} desc="Clap" />
+          <DrumPad button="d" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" clicked={this.clickHandler} desc="Open-HH" />
+          <DrumPad button="z" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" clicked={this.clickHandler} desc="Kick-n'-Hat" />
+          <DrumPad button="x" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" clicked={this.clickHandler} desc="Kick" />
+          <DrumPad button="c" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" clicked={this.clickHandler} desc="Closed-HH" />
         </DrumMachine>
       </div>
     );
--- src/components/Pad.jsx
+++ src/components/Pad.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
 import "./Pad.css"
 
-export default class Pad extends Component {
+export default class DrumPad extends Component {
     clickHandler = (e) => {
+        let display = document.getElementById("display")
         let key = document.getElementsByName(e.target.value.toLowerCase())
         if (key[0]) {
+            display.innerHTML = e.target.value
             key[0].play()
         }
     }
 
     render() {
         return (
-            <div className="drum-pad" >
-                <input type="button" value={this.props.button} onClick={this.clickHandler} />
-                <audio src={this.props.audioSrc} className="clip" name={this.props.button}></audio>
+            <div className="drum-pad" id={this.props.desc}>
+                <button type="button" value={this.props.button} onClick={this.clickHandler} name={this.props.desc}>{this.props.button.toUpperCase()}</button>
+                <audio src={this.props.audioSrc} className="clip" name={this.props.button} id={this.props.button.toUpperCase()}></audio>
             </div>
         )
     }
