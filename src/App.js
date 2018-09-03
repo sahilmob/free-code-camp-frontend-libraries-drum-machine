12638a449d94c0b083efd47f7e4eef55c97647f5
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-03 08:09:26 +0300

    Add all drum pads

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
 
   handleKeyDown = (e) => {
     let key = document.getElementsByName(e.key)
-    key[0].play()
+    if (key[0]) {
+      key[0].play()
+    }
   }
 
   componentDidMount() {
@@ -21,7 +23,15 @@ class App extends Component {
     return (
       <div className="App">
         <DrumMachine>
-          <Pad button="w" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" />
+          <Pad button="q" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" />
+          <Pad button="w" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" />
+          <Pad button="e" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" />
+          <Pad button="a" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" />
+          <Pad button="s" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" />
+          <Pad button="d" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" />
+          <Pad button="z" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" />
+          <Pad button="x" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" />
+          <Pad button="c" audioSrc="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" />
         </DrumMachine>
       </div>
     );
